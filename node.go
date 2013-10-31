1129dab3a942e86a680c42f3a61c238e9947229f
John-Alan Simmons <simmons.johnalan@gmail.com>
2013-10-31 02:58:07 -0400

    Started Node implementation

--- node.go
+++ node.go
@@ -5,7 +5,12 @@ import ()
 // Node
 //	Host		string
 //	ID			[]byte
-// 	sync.Mutex
+// 	lock sync.Mutex
 //	successor	*Node
 //	predessor	*Node
 //	fingers		fingerTable
+
+func NewNode(id NodeID, host string) *Node {
+	node := &Node{ID: id, Host: host}
+	return node
+}

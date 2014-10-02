e37a55ec875506af9a2652c370cc3fcf4af86795
John-Alan Simmons <johnalan.simmons@polylabs.ca>
2014-10-02 01:22:54 -0400

    Added orgin and id params to message, renamed value to body, and implemented generic codecs via DecodeBody()

--- message.go
+++ message.go
@@ -17,17 +17,18 @@ const (
 	PRED_REQ            // A request for a nodes predecessor
 	STATUS_ERROR        // Response indicating an error
 	STATUS_OK           // Simple status OK response
-
 )
 
 // Represents a message in the DHT network
 type Message struct {
+	id      NodeID // Message unique id
 	key     NodeID // Message Key
-	value   []byte // Content of message
-	purpose int    // The purpose of the message
-	sender  NodeID // The node who sent the message
-	target  NodeID // The targer node of the message
-	hops    int    // Number of hops so far taken by the message
+	purpose uint   // The purpose of the message
+	origin  Node   // The node from whom the messages originated
+	sender  Node   // The node who sent the message
+	target  Node   // The targer node of the message
+	hops    uint   // Number of hops so far taken by the message
+	body    []byte // Content of message
 }
 
 // Create a new message
@@ -35,9 +36,9 @@ func (c *Cluster) NewMessage(purpose int, key NodeID, body []byte) *Message {
 	// Sender and Target are filled in by the cluster upon sending the message
 	return &Message{
 		key:     key,
-		value:   body,
+		body:    body,
 		purpose: purpose,
-		sender:  c.self.Id,
+		sender:  c.self,
 		hops:    0,
 	}
 }
@@ -47,8 +48,8 @@ func (msg *Message) Key() NodeID {
 	return msg.key
 }
 
-// Get the message value
-func (msg *Message) Value() []byte {
+// Get the message body
+func (msg *Message) Body() []byte {
 	return msg.value
 }
 
@@ -62,14 +63,24 @@ func (msg *Message) Hops() int {
 	return msg.hops
 }
 
-// Get the message target
-func (msg *Message) Target() NodeID {
+// Get the message origin node
+func (msg *Message) Origin() Node {
+	return msg.origin
+}
+
+// Get the message target node
+func (msg *Message) Target() Node {
 	return msg.target
 }
 
-// Get the message sender
-func (msg *Message) Sender() NodeID {
+// Get the message sender node
+func (msg *Message) Sender() Node {
 	return msg.sender
+}
+
+// Extract the message body into the given value (must be a pointer), using the provided codec
+func (msg *Message) DecodeBody(codec codec.Codec, v interface{}) error {
+	return codec.NewDecoder(bytes.NewBuffer(msg.body)).Decode(v)
 }
 
 // Helper utilies for creating specific messages
